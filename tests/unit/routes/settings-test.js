93ec890376a4998f12a7aaf640aa335202f29e83
Tyler Bratton <tylerbratton96@gmail.com>
2018-10-11 21:14:11 -0400

    "Improve" tests

--- tests/unit/routes/settings-test.js
+++ tests/unit/routes/settings-test.js
@@ -5,7 +5,7 @@ module('Unit | Route | settings', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:settings');
+    const route = this.owner.lookup('route:settings');
     assert.ok(route);
   });
 });
